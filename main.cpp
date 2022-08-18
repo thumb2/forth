5f91b8748017bbe762d07ca5a4bcf6f1f015091e
Zhang Xin <zhangxin.02@foxmail.com>
2022-08-18 11:31:02 +0800

    For chapter 1.

--- main.cpp
+++ main.cpp
@@ -10,10 +10,10 @@ using namespace std;
 bool debug = false;
 
 typedef void(* func_type)();
-func_type func_arr[4096 * 1024];     //
-func_type* here = func_arr;          // HERE in forth language
-func_type* pc;                       // Program Counter
-func_type* lr;                       // Link Register
+func_type func_arr[4096 * 1024];     // 工作区，用于存储词的执行代码等
+func_type* here = func_arr;          // here指针，指向当前空白的工作区单元
+func_type* pc;                       // 程序计数器（Program Counter）
+func_type* lr;                       // 链接寄存器（Link Register）
 
 
 class Word
@@ -25,22 +25,21 @@ public:
         is_immediate_(imme)
     {
     }
-    string name_;
-    func_type* func_;
-    bool is_immediate_;
+    string name_;               // 词的名字
+    func_type* func_;           // 词的执行区域
+    bool is_immediate_;         // 是否是立即词
 };
 
-std::stack<void *> data_stack;
-std::stack<void *> call_stack;
-std::vector<Word> dict;
-string input_word;
+std::stack<void *> data_stack;  // 数据栈（又称参数栈）
+std::stack<void *> call_stack;  // 调用栈（又称返回栈）
+std::vector<Word> dict;         // 词典
+bool is_compiling = false;      // 是否处于编译态
 
-bool is_compiling = false;
-void ret()
-{
-    pc = lr;
-}
+// **************************************************************
+// 以下部分是一些工具
+// **************************************************************
 
+// 输出调试信息
 void print_info(string func_name)
 {
     if (debug) {
@@ -51,6 +50,55 @@ void print_info(string func_name)
     }
 }
 
+// 在C语言中定义词的工具
+void def_word(string name, void (*func)(), bool imme = false)
+{
+    *here = func;
+    dict.push_back(Word(name, here, imme));
+    here++;
+}
+
+// 判断是否是数
+bool is_number(const string &str)
+{
+    for (auto c: str) {
+        if (c < '0' || c > '9') return false;
+    }
+    return true;
+}
+
+// **************************************************************
+// 以下部分是虚拟机相关的代码
+// **************************************************************
+
+// 虚拟机的跳转并链接
+void jump_and_link()
+{
+    print_info(__FUNCTION__);
+    lr = pc + 1;
+    pc = (func_type*)(*pc);
+}
+
+// 虚拟机的返回
+void ret()
+{
+    pc = lr;
+}
+
+// 虚拟机主循环
+void vm_loop()
+{
+    while(true) {
+        print_info(__FUNCTION__);
+        (*pc++)();
+    }
+}
+
+// **************************************************************
+// 以下部分是定义的词
+// **************************************************************
+
+// + (a b -- a+b )
 void plus_func()
 {
     print_info(__FUNCTION__);
@@ -62,6 +110,7 @@ void plus_func()
     ret();
 }
 
+// - (a b -- a-b )
 void minus_func()
 {
     print_info(__FUNCTION__);
@@ -73,6 +122,7 @@ void minus_func()
     ret();
 }
 
+// 弹出并打印栈顶 (a -- )
 void dot_func()
 {
     print_info(__FUNCTION__);
@@ -82,28 +132,14 @@ void dot_func()
     ret();
 }
 
-bool is_number(const string &str)
-{
-    for (auto c: str) {
-        if (c < '0' || c > '9') return false;
-    }
-    return true;
-}
-
-void interpreter();
-
-void jump_and_link()
-{
-    print_info(__FUNCTION__);
-    lr = pc + 1;
-    pc = (func_type*)(*pc);
-}
+// 执行时的:
 void do_colon()
 {
     print_info(__FUNCTION__);
     call_stack.push((void *)lr);
 }
 
+// 编译时的:
 void colon_func()
 {
     is_compiling = true;
@@ -113,6 +149,8 @@ void colon_func()
     *here++ = do_colon;
     ret();
 }
+
+// 执行时的;
 void do_semicolon()
 {
     print_info(__FUNCTION__);
@@ -121,6 +159,7 @@ void do_semicolon()
     ret();
 }
 
+// 执行时的;
 void semicolon_func()
 {
     is_compiling = false;
@@ -128,6 +167,7 @@ void semicolon_func()
     ret();
 }
 
+// 执行时的literal（读取数）
 void do_lit()
 {
     print_info(__FUNCTION__);
@@ -135,46 +175,14 @@ void do_lit()
     pc++;
 }
 
-void branch_func()
-{
-    pc = (func_type*)(*pc);
-}
+// **************************************************************
+// 以下部分是编译、解释器
+// **************************************************************
 
-void zero_branch_func()
-{
-    int top = (long int)(data_stack.top());
-    data_stack.pop();
-    pc = top == 0 ? (func_type*)(*pc) : pc + 1;
-}
-
-void here_func()
-{
-    data_stack.push((void *)(long int)(here));
-    ret();
-}
-
-void fetch_func()
-{
-    print_info(__FUNCTION__);
-    long int top = (long int)(data_stack.top());
-    data_stack.pop();
-    data_stack.push((void*)(*((long int *)(top))));
-    ret();
-}
-
-void store_func()
-{
-    print_info(__FUNCTION__);
-    long int* address = (long int *)(data_stack.top());
-    data_stack.pop();
-    long int value  = (long int)(data_stack.top());
-    data_stack.pop();
-    *address = value;
-    ret();
-}
-
+// 编译、解释器
 void interpreter()
 {
+    string input_word;
     bool is_word_found = false;
     Word* word;
     cin >> input_word;
@@ -217,11 +225,17 @@ void interpreter()
     ret();
 }
 
+// 不一定会到这里，但到这里的话，就回到interpreter
 void loop_interpreter()
 {
     pc = func_arr;
 }
 
+// **************************************************************
+// 以下部分是初始化和程序入口
+// **************************************************************
+
+// 初始化
 void setup()
 {
     here = func_arr;
@@ -229,21 +243,6 @@ void setup()
     *here++ = loop_interpreter;
     pc = func_arr;
     lr = func_arr;
-}
-
-void main_loop()
-{
-    while(true) {
-        print_info("main loop");
-        (*pc++)();
-    }
-}
-
-void def_word(string name, void (*func)(), bool imme = false)
-{
-    *here = func;
-    dict.push_back(Word(name, here, imme));
-    here++;
 }
 
 int main()
@@ -254,11 +253,5 @@ int main()
     def_word(".", dot_func);
     def_word(":", colon_func);
     def_word(";", semicolon_func, true);
-    def_word("branch", branch_func);
-    def_word("0branch", zero_branch_func);
-    def_word("here", here_func);
-    def_word("@", fetch_func);
-    def_word("!", store_func);
-
-    main_loop();
+    vm_loop();
 }
